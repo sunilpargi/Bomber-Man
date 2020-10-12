855073bf7dc9c57d3f2184a881d63720c7fe241b
rahulpargi641 <61396833+rahulpargi641@users.noreply.github.com>
2020-10-12 11:57:31 +0530

    added player movement

---
+++ Bomber Man/Assets/Scripts/Player.cs
@@ -0,0 +1,72 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float speed = 10f;
+    private float maxX = 2.55f;
+    private float minX = -2.55f;
+   
+
+    // Update is called once per frame
+    void Update()
+    {
+        float h = Input.GetAxis("Horizontal");
+        Vector2 temp = transform.position;
+        if(h > 0)
+        {
+            temp.x += speed * Time.deltaTime;
+        }
+        else if(h < 0)
+        {
+            temp.x -= speed * Time.deltaTime;
+        }
+
+        if(temp.x < minX) temp.x = minX;      
+        if (temp.x > maxX) temp.x = maxX;
+        
+        transform.position = temp;
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}//class
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
